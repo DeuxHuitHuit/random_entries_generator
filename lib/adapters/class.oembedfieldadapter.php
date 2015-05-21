f86368bc713166bf9b2c071a559798c46368e14b
Nicolas Brassard <nico@nitriques.com>
2015-05-21 18:04:48 -0400

    Added oembed field adapter

---
+++ lib/adapters/class.oembedfieldadapter.php
@@ -0,0 +1,54 @@
+<?php
+    /*
+    Copyight: Deux Huit Huit 2015
+    LICENCE: MIT http://deuxhuithuit.mit-license.org;
+    */
+    
+    if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+    
+    /**
+     *
+     * @author Deux Huit Huit
+     * https://deuxhuithuit.com/
+     *
+     */
+    class oembedFieldAdapter extends FieldAdapter
+    {
+        private static $randomness = array(
+            'YouTube' => array(
+                'https://www.youtube.com/watch?v=HPPj6viIBmU',
+                'https://www.youtube.com/watch?v=H-qmZ_J7WGc',
+                'https://www.youtube.com/watch?v=2QqE4iRTqo0',
+            ),
+            'Vimeo' => array(
+                'https://vimeo.com/128395258',
+                'https://vimeo.com/123757684',
+                'https://vimeo.com/3631454',
+            )
+        );
+        public function type()
+        {
+            return 'oembed';
+        }
+        public static function getUrl($drivers) {
+            foreach (self::$randomness as $driver => $values) {
+                if (in_array($driver, $drivers)) {
+                    return $values[array_rand($values, 1)];
+                }
+            }
+            return null;
+        }
+        public function data($section, $field)
+        {
+            $status;
+            $url = static::getUrl(explode(',', $field->get('driver')));
+            if ($url == null) {
+                return null;
+            }
+            $data = $field->processRawFieldData($url, $status);
+            if ($status != Field::__OK__) {
+                return null;
+            }
+            return $data;
+        }
+    }
