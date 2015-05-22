5be70d9b5fb57d63e2fa28bdd6c3b4e18615d6fd
nitriques <nico@nitriques.com>
2015-05-21 23:06:27 -0400

    fixed licence in header

--- lib/adapters/class.datefieldadapter.php
+++ lib/adapters/class.datefieldadapter.php
@@ -1,7 +1,7 @@
 <?php
     /*
     Copyrights: Deux Huit Huit 2015
-    LICENCE: MIT http://deuxhuithuit.mit-license.org;
+    LICENCE: MIT, http://deuxhuithuit.mit-license.org/
     */
     
     if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
