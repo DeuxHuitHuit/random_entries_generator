bb633e8ed12401dc55079c3ec3033b8329a1e23f
Nicolas Brassard <nico@nitriques.com>
2015-05-26 16:28:44 -0400

    Make ImageUploadFieldAdapter use ImageGenerator

--- lib/adapters/class.imageuploadfieldadapter.php
+++ lib/adapters/class.imageuploadfieldadapter.php
@@ -21,11 +21,7 @@
             return 'image_upload';
         }
 
-        public function data($section, $field)
-        {
-            if (self::$seed == null) {
-                self::$seed = time();
-            }
+        protected function getFilename($field) {
             $validator = $field->get('validator');
             $filename = 'image-%s';
             if (preg_match('/jpe?\??g/i', $validator) == 1) {
@@ -43,10 +39,41 @@
             else {
                 $filename .= '.bmp';
             }
-            $filename = sprintf($filename, self::$seed++);
+            return sprintf($filename, self::$seed++);
+        }
+
+        protected function findValue($field, $value, $default) {
+            $min = General::intval($field->get('min_' . $value));
+            $max = General::intval($field->get('max_' . $value));
+            if ($max > 0) {
+                return $max;
+            }
+            if ($min > 0) {
+                return $min;
+            }
+            return $default;
+        }
+
+        public function data($section, $field)
+        {
+            if (self::$seed == null) {
+                self::$seed = time();
+            }
+            $filename = $this->getFilename($field);
             $filepath = $field->getFilePath($filename);
             touch($filepath);
             $type = General::getMimeType($filepath);
+            $width = $this->findValue($field, 'width', 1920);
+            $height = $this->findValue($field, 'height', 1080);
+            $image = ImageGenerator::generate(array(
+                'width' => $width,
+                'height' => $height,
+                'filepath' => $filepath,
+                'type' => $type,
+            ));
+            if (!$image) {
+                throw new Exception("Failed to download random image");
+            }
             return array(
                 'file' => $filename,
                 'mimetype' => $type,
--- lib/generators/class.imagegenerator.php
+++ lib/generators/class.imagegenerator.php
@@ -1,9 +1,9 @@
 <?php
+
     /*
     Copyrights: Deux Huit Huit 2015
     LICENCE: MIT, http://deuxhuithuit.mit-license.org/
     */
-    
     if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
     
     /**
@@ -17,14 +17,49 @@
         protected static $defaultOptions = array(
             'width' => 1920,
             'height' => 1080,
-            'filename' => 'image.jpg',
+            'filepath' => '/tmp/image.jpg',
             'type' => 'jpg',
             'url-format' => 'http://placehold.it/%sx%s/%s/%s.%s'
         );
+
+        public static function fixColor($hexColor)
+        {
+            return str_replace('#', '', $hexColor);
+        }
+
+        public static function request($url, $filepath)
+        {
+            $ch = curl_init($url);
+            $fp = fopen($filepath, "w");
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+            curl_setopt( $ch, CURLOPT_HEADER, false );
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+            curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+            curl_setopt( $ch, CURLOPT_USERAGENT, Symphony::Configuration()->get('useragent', 'general'));
+            $content = curl_exec( $ch );
+            $status = curl_getinfo( $ch );
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $success = $httpCode == 200;
+            if ($success) {
+                fwrite($fp, $content);
+            }
+            curl_close( $ch );
+            fclose($fp);
+            return $success;
+        }
+
         public static function generate($options)
         {
             $value = array();
             $options = array_merge($options, self::$defaultOptions, $options);
-            
+            $faker = \Faker\Factory::create();
+            $foregroundcolor = static::fixColor($faker->hexcolor);
+            $backgroundcolor = static::fixColor($faker->hexcolor);
+            $url = sprintf($options['url-format'],
+                $options['width'], $options['height'],
+                $backgroundcolor, $foregroundcolor,
+                $type
+            );
+            return static::request($url, $options['filepath']);
         }
     }
