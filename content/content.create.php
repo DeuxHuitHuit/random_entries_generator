94c66d6780006cbddca26921d0acacc79cf525c5
ChoboHub <antoine_lucas_999@Hotmail.com>
2016-08-08 16:42:54 -0400

    Hot fix (author id was missing)

--- content/content.create.php
+++ content/content.create.php
@@ -39,6 +39,7 @@
             if (isset($_GET['s']) && !empty($_GET['s'])) {
                 $s = General::sanitize($_GET['s']);
                 $sectionId = 0;
+                $authorId = Symphony::Author()->get('id');
                 $section = NULL;
                 if (ctype_digit($s)) {
                     $sectionId = General::intval($s);
@@ -56,6 +57,7 @@
                 }
                 $fields = $section->fetchFields();
                 $entry = EntryManager::create();
+                $entry->set('author_id', $authorId);
                 $entry->set('section_id', $sectionId);
                 foreach ($fields as $field) {
                     $adapter = FieldAdapterManager::get($field->get('type'));
